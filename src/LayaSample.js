c5da02196675e9dae59ff33e10d85010f138e11b
Jack Chen <920901835@qq.com>
2017-12-20 13:02:08 +0800

    syn

--- src/LayaSample.js
+++ src/LayaSample.js
@@ -1,6 +1,6 @@
 
 var tiledMap;
-
+var Handler= Laya.Handler;
 
 
 // 创建TestPageUI的子类
@@ -20,7 +20,7 @@ function Test()
 	(function()
 	{
 		// 不支持WebGL时自动切换至Canvas
-		Laya.init(1100, 950, WebGL);
+		Laya.init(1400, 950, WebGL);
 
         //调用DebugTool调试面板
         // Laya.DebugTool.init();
@@ -36,21 +36,27 @@ function Test()
 	function createMap()
 	{
 		tiledMap = new TiledMap();
-		tiledMap.createMap("../laya/assets/map/orthogonal-test-movelayer.json", new Rectangle(0, 0, Laya.stage.width, Laya.stage.height), null);
+		tiledMap.createMap("../laya/assets/map/orthogonal-test-movelayer.json", new Rectangle(0, 0, 1100, 950),Handler.create(this,onMapLoaded));
         Laya.stage.on(laya.events.Event.KEY_DOWN,this,onKeyDown);//设置键盘监听事件
-    
 	}
 })();
 
+function onMapLoaded(){
+    //预加载资源文件后执行回调
+    Laya.loader.load(["res/atlas/comp.atlas","res/atlas/template/ButtonTab.atlas"], Handler.create(this, onDialogLoaded));
+}
+
+function onDialogLoaded(){
+    Laya.class(Test, "Test", TestUI);
+    var view = new Test();
+    Laya.stage.addChild(view);
+}
 
 /**
  * 点击事件监听
  * @param {*} e 
  */
 function onKeyDown(e) {
-        Laya.class(Test, "Test", TestUI);
-        var view = new Test();
-        Laya.stage.addChild(view);
     var player = tiledMap.getLayerObject("hero","role1");
     var b = true;
 //     var b = map1.getTileProperties(0, a-1, "isCanPass");
