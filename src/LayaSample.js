10e47b26f229781479d4d2e9e04d53df00fdb0b9
Jack Chen <920901835@qq.com>
2017-12-22 14:36:46 +0800

    syn

--- src/LayaSample.js
+++ src/LayaSample.js
@@ -1,6 +1,7 @@
-
+//导包
 var tiledMap;
 var operationView;
+var questionDialog;
 var Handler= Laya.Handler;
 var Stage     = Laya.Stage;
 var TiledMap  = Laya.TiledMap;
@@ -10,22 +11,33 @@ var Event = Laya.Event;
 var Socket = Laya.Socket;
 var Byte   = Laya.Byte;
 
-//Global Variable
-var curUserId;
-var positions=[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
-
 // 定义常量
 var GAME_READY = 0;
 var GAME_DICE_RESULT = 1;
 var GAME_ANSWERING_QUESTION = 2;
 var GAME_ANSWER_QUESTION_RESULT = 3;
 var GAME_OVER = 4;
+var ROOM_WAITING = 0;
+var ROOM_PLAYING = 1;
 
+//View Global Variable
+var traceBits=[1,1,2,2,1,1,1,0,0,1,1,1,1,1,2,2,2,3,3,3,2,2,2,1,1,1,2,2,2,2,3,3,3,3,3,3,0,0,0,0,3,3,2,2,2,2,3,3,0,0,0,0,0,0,0,0,0,0];
+var graph;
+var unit_x=100;
+var unit_y=80;
+var roles;
 
-// 创建TestPageUI的子类
+//Game Data Variables
+var myUserId;
+var players;
+
 function OperationPanel()
 {
 	OperationPanel.super(this);   
+}
+function QuestionDialog()
+{
+	QuestionDialog.super(this);
 }
 
 (function()
@@ -123,84 +135,138 @@ function onConnectError(e)
 
 function refreshUI(message){
     if(typeof message.nickName !== 'undefined'){
-        curUserId = message.id;
+        //socket连接数据包，更新玩家ID
+        myUserId = message.id;
         return;
     }
+    if(message.status === ROOM_PLAYING){
+        refreshPlayerPanel(message);
+        refreshPlayerPosition(message);
+        refreshOpeartionpanel(message);
+    } else if (message.status === ROOM_WAITING){
+        
+    }else{
+        console.log("房间状态不正确！");
+    }
+}
+
+/**
+ * 刷新操作按钮
+ * @param {*} message 
+ */
+function refreshOpeartionpanel(message){
     var stage = message.game.stage;
-    var curPlayer = message.game.currentPlayerId;
-    refreshPlayerPanel(message);
+    var curPlayer;
+    $.each(message.playerList,function(index,item){
+        if(item.id === message.game.currentPlayerId) {
+            curPlayer = item.userId;
+            return false;
+        }
+    });
+    setBtnVisibility(false,false,false);
     switch(stage){
         case GAME_READY:
-        //新一轮游戏就绪
-        if(curPlayer===curUserId){
-            setBtnVisibility(false,false,true);
-        }else{
-            setBtnVisibility(false,false,false);
-        }
+            //新一轮游戏就绪 UI数据包
+            if(curPlayer===myUserId){
+                setBtnVisibility(false,false,true);
+            }
         break;
         case GAME_DICE_RESULT:
-        //掷骰子点数
-        var dice = $("#dice");
-        dice.attr("class","dice");//清除上次动画后的点数 
-        dice.css("cursor","default"); 
-        $(".wrap").append("<div id='dice_mask'></div>");//加遮罩 
-        var num = message.game.diceNumber;
-        dice.animate({left: '+2px'}, 100,function(){ 
-            dice.addClass("dice_t"); 
-        }).delay(200).animate({top:'-2px'},100,function(){ 
-            dice.removeClass("dice_t").addClass("dice_s"); 
-        }).delay(200).animate({opacity: 'show'},600,function(){ 
-            dice.removeClass("dice_s").addClass("dice_e"); 
-        }).delay(100).animate({left:'-2px',top:'2px'},100,function(){ 
-            dice.removeClass("dice_e").addClass("dice_"+num); 
-            $("#result").html("掷得点数是<span>"+num+"</span>"); 
-            console.log(num);
-            dice.css('cursor','pointer'); 
-            $("#dice_mask").remove();//移除遮罩 
-        }); 
-        if(curPlayer===curUserId){
+            //掷骰子点数结果 UI数据包
+            var dice = $("#dice");
+            dice.attr("class","dice");//清除上次动画后的点数 
+            dice.css("cursor","default"); 
+            $(".wrap").append("<div id='dice_mask'></div>");//加遮罩 
+            var num = message.game.diceNumber;
+            dice.animate({left: '+2px'}, 100,function(){ 
+                dice.addClass("dice_t"); 
+            }).delay(200).animate({top:'-2px'},100,function(){ 
+                dice.removeClass("dice_t").addClass("dice_s"); 
+            }).delay(200).animate({opacity: 'show'},600,function(){ 
+                dice.removeClass("dice_s").addClass("dice_e"); 
+            }).delay(100).animate({left:'-2px',top:'2px'},100,function(){ 
+                dice.removeClass("dice_e").addClass("dice_"+num); 
+                $("#result").html("掷得点数是<span>"+num+"</span>"); 
+                console.log(num);
+                dice.css('cursor','pointer'); 
+                $("#dice_mask").remove();//移除遮罩 
+            }); 
+            if(curPlayer===myUserId){
 
-        }else{
-            
-        }
+            }else{
+                
+            }
         break;
         case GAME_ANSWERING_QUESTION:
-        //获取问题中（回答中）
-        if(curPlayer===curUserId){
+            //获取问题中（回答中） UI数据包
+            if(curPlayer===myUserId){
 
-        }else{
-            
-        }
+            }else{
+                
+            }
         break;
         case GAME_ANSWER_QUESTION_RESULT:
-        //回答问题结果
-        if(curPlayer===curUserId){
+            //回答问题结果 UI数据包
+            if(curPlayer===myUserId){
 
-        }else{
-            
-        }
+            }else{
+                
+            }
         break;
         case GAME_OVER:
-        //游戏结束
+            //游戏结束 UI数据包
+            setBtnVisibility(true,true,false);
+            //TODO:游戏结果弹窗
         break;
     }
 }
 
 /**
+ * 刷新玩家位置
+ * @param {*} obj 
+ */
+function refreshPlayerPosition(obj){
+    if(obj.status === ROOM_PLAYING){
+        $.each(obj.playerList,function(index,item){
+            if(item.position === -1) item.position = 0;
+            var position =graph[item.position];
+            roles[index].visible = true;
+            roles[index].x=position.x;
+            roles[index].y=position.y;
+        });
+    }else if(obj.status === ROOM_WAITING){
+        $.each(obj.playerList,function(index,item){
+            if(item.position === -1) item.position = 0;
+            var position = graph[item.position];
+            roles[index].x=position.x;
+            roles[index].y=position.y; 
+        });
+    }
+}
+
+/**
  * 刷新玩家界面
- * @param {*} message 
+ * @param {*数据包} message 
  */
 function refreshPlayerPanel(obj){
     console.log(obj);
-    if(typeof operationView ==='undefined'){
-        return;
+    if(obj.status === ROOM_PLAYING){
+        operationView.playerText.text = " \t昵称\t \t\t \t位置\t \t\t \t金币\n\n";
+        $.each(obj.playerList,function(index,item){
+            operationView.playerText.text += " \t\t"+item.nickName+"\t";
+            operationView.playerText.text += " \t\t\t\t"+item.position+"\t";
+            operationView.playerText.text += " \t\t\t\t"+item.balance+"\n\n";
+        });
+    }else if(obj.status === ROOM_WAITING){
+        operationView.playerText.text = " \t昵称\t \t\t \t位置\t \t\t \t金币\n\n";
+        $.each(obj.playerList,function(index,item){
+            operationView.playerText.text += " \t\t"+item.nickName+"\t";
+            operationView.playerText.text += " \t\t\t\t"+ 0 +"\t";
+            operationView.playerText.text += " \t\t\t\t"+ 0 +"\n\n";
+        });
+    } else {
+        console.log("房间状态异常=====");
     }
-    operationView.playerText.text = " \t昵称\t \t\t \t位置\t \t\t \t金币\n\n";
-    $.each(obj.playerList,function(index,item){
-        operationView.playerText.text += " \t\t"+item.nickName+"\t";
-        operationView.playerText.text += " \t\t\t\t"+item.position+"\t";
-        operationView.playerText.text += " \t\t\t\t"+item.balance+"\n\n";
-    });
 }
 
 /**
@@ -222,8 +288,6 @@ function createMap()
 {
     tiledMap = new TiledMap();
     tiledMap.createMap("../laya/assets/map/orthogonal-test-movelayer.json", new Rectangle(0, 0, 1100, 950),Handler.create(this,onMapLoaded));
-    tiledMap.gridSize = 20;
-    Laya.stage.on(laya.events.Event.KEY_DOWN,this,onKeyDown);//设置键盘监听事件
 }
 
 /**
@@ -231,9 +295,38 @@ function createMap()
  */
 function onMapLoaded(){
     this.tiledMap.scale = 1.0;
+    //初始化地图走向
+    graph = [];
+    $.each(traceBits,function(index,item){
+        if(index === 0) {
+            graph.push({"x":50,"y":40});
+            return true;
+        }
+        var position = graph[index-1];
+        var t = traceBits[index-1];
+        switch(t){
+            case 0:
+            graph.push({"x":position.x,"y":position.y-unit_y});
+            break;
+            case 1:
+            graph.push({"x":position.x+unit_x,"y":position.y});
+            break;
+            case 2:
+            graph.push({"x":position.x,"y":position.y+unit_y});
+            break;
+            case 3:
+            graph.push({"x":position.x-unit_x,"y":position.y});
+            break;
+        }
+    });
+    //初始化角色列表
+    roles = [tiledMap.getLayerObject("hero","role1"),tiledMap.getLayerObject("hero","role2"),
+            tiledMap.getLayerObject("hero","role3"),tiledMap.getLayerObject("hero","role4")];
+    $.each(roles,function(index,item){
+        item.visible = false;
+    });
     //预加载资源文件后执行回调
     Laya.loader.load(["h5/res/atlas/comp.atlas","h5/res/atlas/template/ButtonTab.atlas","h5/res/atlas/template/Warn.atlas"], Handler.create(this, onDialogLoaded));
-    
 }
 
 /**
@@ -241,7 +334,10 @@ function onMapLoaded(){
  */
 function onDialogLoaded(){
     Laya.class(OperationPanel, "OperationPanel", OperationPanelUI);
+    Laya.class(QuestionDialog, "QuestionDialog", QuestionDialogUI);
     operationView = new OperationPanel();
+    questionDialog = new QuestionDialog();
+    questionDialog.visible = true;
     operationView.x = 1100;
     operationView.y = 0;
     operationView.btnReady.on(Event.CLICK, this, btnReadyClicked);
@@ -294,41 +390,4 @@ function btnCancelReadyClicked(){
             }
         }
     });
-}
-
-/**
- * 键盘事件监听
- */
-function onKeyDown(e) {
-    var player = tiledMap.getLayerObject("hero","role1");
-    var b = true;
-    var height = tiledMap.height;
-    var width = tiledMap.width;
-    var gridSize = tiledMap.width/tiledMap.numColumnsTile;
-　　switch (e.keyCode) {
-　　　　case 38:{
-　　　　　　if (((player.y - gridSize) > 0) && b) {
-　　　　　　　　player.y -= gridSize;
-           }
-　　　　　　break;
-      }
-      case 40:{
-          if (((player.y + gridSize) < (height- gridSize)) && b){
-　　　　　　　　player.y += gridSize;
-          }
-          break;
-      }  
-      case 37:{
-　　　　　　if (((player.x - gridSize) > 0) && b) {
- 　　　　　　　player.x -= gridSize;
-　　　　　　}
-　　　　　　break;
-      }
-      case 39:{
-　　　　　　if (((player.x + gridSize) < (width - gridSize)) && b) {
-　　　　　　　　player.x += gridSize;
-　　　　　　}
-　　　　    break;
-　　　　}　　
-　　}
 }
--- src/ui/layaUI.max.all.js
+++ src/ui/layaUI.max.all.js
@@ -5,10 +5,10 @@ var Dialog=laya.ui.Dialog;
 var OperationPanelUI=(function(_super){
 		function OperationPanelUI(){
 			
-		    this.btnExit=null;
+		    this.playerText=null;
 		    this.btnReady=null;
 		    this.btnDice=null;
-		    this.playerText=null;
+		    this.btnExit=null;
 
 			OperationPanelUI.__super.call(this);
 		}
@@ -22,6 +22,26 @@ var OperationPanelUI=(function(_super){
 
 		}
 
-		OperationPanelUI.uiView={"type":"View","props":{"x":0,"width":400,"height":700},"child":[{"type":"Tab","props":{"y":582,"x":213,"width":458,"selectedIndex":2,"pivotY":84,"pivotX":228,"height":167},"child":[{"type":"Button","props":{"y":102,"x":236,"width":143,"var":"btnExit","skin":"template/ButtonTab/btn_LargeTabButton_Right.png","name":"btnExit","labelSize":20,"labelColors":"#007AFF,#007AFF,#FFFFFF","label":"退出","height":66}},{"type":"Button","props":{"y":102,"x":52,"width":143,"var":"btnReady","skin":"template/ButtonTab/btn_LargeTabButton_Right.png","name":"btnReady","labelSize":20,"labelColors":"#007AFF,#007AFF,#FFFFFF","label":"准备","height":66}},{"type":"Button","props":{"y":102,"x":138,"width":143,"visible":false,"var":"btnDice","skin":"template/ButtonTab/btn_LargeTabButton_Right.png","name":"btnDice","labelSize":20,"labelColors":"#007AFF,#007AFF,#FFFFFF","label":"掷骰子","height":66}}]},{"type":"Image","props":{"y":55,"x":29,"width":342,"skin":"template/Warn/alert_dialog.png","height":234}},{"type":"TextArea","props":{"y":77,"x":56,"width":290,"var":"playerText","text":"TextArea","height":192,"fontSize":20,"editable":false}}]};
+		OperationPanelUI.uiView={"type":"View","props":{"x":0,"width":400,"height":700},"child":[{"type":"Image","props":{"y":55,"x":29,"width":342,"skin":"template/Warn/alert_dialog.png","height":234}},{"type":"TextArea","props":{"y":77,"x":56,"width":290,"var":"playerText","text":"TextArea","height":192,"fontSize":20,"editable":false}},{"type":"Button","props":{"y":600,"x":40,"width":100,"var":"btnReady","skin":"comp/button.png","name":"btnReady","labelSize":20,"labelFont":"SimHei","label":"准备","height":50}},{"type":"Button","props":{"y":600,"x":150,"width":100,"var":"btnDice","skin":"comp/button.png","name":"btnDice","labelSize":20,"labelFont":"SimHei","label":"掷骰子","height":50}},{"type":"Button","props":{"y":600,"x":260,"width":100,"var":"btnExit","skin":"comp/button.png","name":"btnExit","labelSize":20,"labelFont":"SimHei","label":"退出","height":50}}]};
 		return OperationPanelUI;
 	})(View);
+var QuestionDialogUI=(function(_super){
+		function QuestionDialogUI(){
+			
+		    this.btnConfirm=null;
+
+			QuestionDialogUI.__super.call(this);
+		}
+
+		CLASS$(QuestionDialogUI,'ui.QuestionDialogUI',_super);
+		var __proto__=QuestionDialogUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(QuestionDialogUI.uiView);
+
+		}
+
+		QuestionDialogUI.uiView={"type":"Dialog","props":{"width":600,"height":400},"child":[{"type":"Image","props":{"y":105,"x":121,"width":338,"skin":"comp/bg.png","height":266}},{"type":"RadioGroup","props":{"y":212,"x":156,"width":320,"space":20,"skin":"comp/radiogroup.png","selectedIndex":0,"labels":"选项A,label2,label2,label2","labelPadding":"0,0,0,10","labelFont":"SimHei","height":150,"direction":"vertical"}},{"type":"Label","props":{"y":159,"x":151,"width":281,"text":"校园中的毛爷爷雕塑指向哪里？","height":27,"font":"SimHei","color":"#ffffff","align":"left"}},{"type":"Button","props":{"y":325,"x":355,"width":72,"var":"btnConfirm","skin":"comp/button.png","name":"btnConfirm","labelSize":15,"labelFont":"SimHei","label":"确定","height":27}},{"type":"Label","props":{"y":126,"x":150,"text":"问题:","fontSize":16,"font":"SimHei","color":"#ffffff"}}]};
+		return QuestionDialogUI;
+	})(Dialog);
